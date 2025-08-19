434c82666d06035ba7fa530f075f5b0afcc70d7d
goncalofgalmeida <goncalofgalmeida@gmail.com>
2025-08-19 23:03:03 +0100

    Correctly display fetched price and formatted date

--- public/js/match-renderer.js
+++ public/js/match-renderer.js
@@ -5,6 +5,53 @@ function renderPlayer(player, isConfirmed) {
   return li;
 }
 
+// TODO: Move function to utils/time.js
+function formatCustomPortugueseDate(isoString) {
+	let startDate;
+	let endDate;
+	let options;
+	let formatter;
+	let parts;
+	let datePartsMap;
+	let weekday;
+	let month;
+	let startTime;
+	let endTime;
+	let endHour;
+	let endMinute;
+
+  monthNames = [
+		'jan', 'fev', 'mar', 'abr', 'mai', 'jun',
+		'jul', 'ago', 'set', 'out', 'nov', 'dez'
+	];
+
+	startDate = new Date(isoString);
+	endDate = new Date(startDate.getTime());
+	endDate.setUTCHours(endDate.getUTCHours() + 1);
+
+	options = {
+		weekday: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false,
+		timeZone: 'UTC'
+	};
+	formatter = new Intl.DateTimeFormat('pt-PT', options);
+	parts = formatter.formatToParts(startDate);
+	datePartsMap = {};
+	parts.forEach(function (part) {
+		datePartsMap[part.type] = part.value;
+	});
+	weekday = datePartsMap.weekday.replace('-feira', '');
+	month = monthNames[startDate.getUTCMonth()];
+	startTime = `${datePartsMap.hour}h${datePartsMap.minute}`;
+	endHour = String(endDate.getUTCHours()).padStart(2, '0');
+	endMinute = String(endDate.getUTCMinutes()).padStart(2, '0');
+	endTime = `${endHour}h${endMinute}`;
+	return `${weekday}, ${datePartsMap.day} ${month} - ${startTime} às ${endTime}`;
+}
+
 async function renderMatchInfo() {
   const pathParts = window.location.pathname.split('/');
   const matchId = pathParts[2];
@@ -21,8 +68,10 @@ async function renderMatchInfo() {
     const match = await response.json();
 
     document.getElementById('match-title').textContent = match.matchTitle;
+    document.getElementById('price').textContent = `${match.pricePerPlayer}€ p/ pessoa`;
     document.getElementById('match-location').textContent = match.location.name;
     document.getElementById('match-location').href = match.location.mapsUrl;
+    document.getElementById('date-and-time').textContent = formatCustomPortugueseDate(match.date);
 
     // team A
     const teamAName = document.getElementById('team-a-name');
